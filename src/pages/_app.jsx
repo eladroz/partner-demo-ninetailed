32c162aca06fc2578e8bf23f5e9cec44b3453e6b
Stackbit Code Editor <projects@stackbit.com>
2022-08-03 09:01:31 +0000

    _app.jsx: changed

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,7 +6,7 @@ import { VariantChoicesProvider } from "../utils/ninetailed";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NinetailedProvider clientId={process.env.NINETAILED_API_KEY}>
+    <NinetailedProvider clientId={process.env.NINETAILED_API_KEY} environment=>
       <VariantChoicesProvider>
         <Component {...pageProps} />
         <Toaster position="bottom-right" />
