b2f97bb0adeae9d518cc064977337cab3b20d954
Stackbit Code Editor <projects@stackbit.com>
2022-08-03 09:01:14 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     reactStrictMode: true,
     env: {
         NINETAILED_API_KEY: process.env.NINETAILED_API_KEY,
-        NINETAILED_ENVIRONMENT: process.env.NINETAILED_API_KEY,
+        NINETAILED_ENVIRONMENT: process.env.NINETAILED_ENVIRONMENT,
     }
 };
